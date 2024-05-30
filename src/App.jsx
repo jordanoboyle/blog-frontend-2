20fd92a5aafc22040d4d8faad2792486e6e55e44
jordanoboyle <jmcmcode@gmail.com>
2024-05-29 21:35:08 -0600

    Sample new posts, function for header, alterations to p tags

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+function Header() {
+  return(
+  <header>
+    <a href="#">Main Menu</a> | <a href="#posts-index">Game Save Data</a> | <a href="#posts-new">New Game</a>
+  </header>
+)}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+     <Header />
+
+      <div id="posts-new">
+        <h1>New post</h1>
+        <form>
+          <div>
+            Post Title: <input type="text"/>
+          </div>
+          <div>
+            Date Entry: <input type="text" />
+          </div>
+          <div>
+            Record Game Data Here: <input type="text" />
+          </div>
+          <button type="submit">SAVE YOUR GAME</button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div id="posts-index">
+        <h1>All posts</h1>
+        <div className="posts">
+          <h2>Game 1</h2>
+          <p>Date: month day year</p>
+          <img src="https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/1708091/capsule_616x353.jpg?t=1706638660"/>
+          <p>
+            Here is some information about Halo Infinite!
+          </p>
+          <button className="button1" type="button">Gameplay</button>
+          <h2>Super Contra</h2>
+          <p>Date: month day year</p>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbE3qwmloOV1eCR6WLcjSlD5kk36DftHQ-EQ&s"/>
+          <p>
+            Here is some information about contra
+          </p>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <footer>
+        <p>Copyright 20XX</p>
+        <p>Content from many contributors</p>
+        <p>Site by: Jordan O'Boyle</p>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
