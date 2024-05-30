9d3b06f8aeb98f93e0fd7ca5d4a04ea94d92da04
jordanoboyle <jmcmcode@gmail.com>
2024-05-29 21:40:23 -0600

    functions, header, PostsNew, PostsIndex, Footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,8 @@ function Header() {
   </header>
 )}
 
-function App() {
+function NewPosts () {
   return (
-    <div>
-     <Header />
-
       <div id="posts-new">
         <h1>New post</h1>
         <form>
@@ -25,31 +22,49 @@ function App() {
           <button type="submit">SAVE YOUR GAME</button>
         </form>
       </div>
+  )
+}
 
-      <div id="posts-index">
-        <h1>All posts</h1>
-        <div className="posts">
-          <h2>Game 1</h2>
-          <p>Date: month day year</p>
-          <img src="https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/1708091/capsule_616x353.jpg?t=1706638660"/>
-          <p>
-            Here is some information about Halo Infinite!
-          </p>
-          <button className="button1" type="button">Gameplay</button>
-          <h2>Super Contra</h2>
-          <p>Date: month day year</p>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbE3qwmloOV1eCR6WLcjSlD5kk36DftHQ-EQ&s"/>
-          <p>
-            Here is some information about contra
-          </p>
-        </div>
+function PostsIndex () {
+  return (
+    <div id="posts-index">
+      <h1>All posts</h1>
+      <div className="posts">
+        <h2>Game 1</h2>
+        <p>Date: month day year</p>
+        <img src="https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/1708091/capsule_616x353.jpg?t=1706638660"/>
+        <p>
+          Here is some information about Halo Infinite!
+        </p>
+        <button className="button1" type="button">Gameplay</button>
+        <h2>Super Contra</h2>
+        <p>Date: month day year</p>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbE3qwmloOV1eCR6WLcjSlD5kk36DftHQ-EQ&s"/>
+        <p>
+          Here is some information about contra
+        </p>
       </div>
+    </div>
+  )
+}
+function Footer () {
+  return (
+    <footer>
+      <p>Copyright 20XX</p>
+      <p>Content from many contributors</p>
+      <p>Site by: Jordan O'Boyle</p>
+    </footer>
 
-      <footer>
-        <p>Copyright 20XX</p>
-        <p>Content from many contributors</p>
-        <p>Site by: Jordan O'Boyle</p>
-      </footer>
+  )
+}
+
+function App() {
+  return (
+    <div>
+     <Header />
+     <NewPosts />
+     <PostsIndex />
+     <Footer />
     </div>
   );
 }
