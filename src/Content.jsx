09e18b7c9f171130280626c18785ad20f69a8a92
jordanoboyle <jmcmcode@gmail.com>
2024-05-29 22:45:55 -0600

    axios imported, axios get

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,26 +1,24 @@
 import { NewPosts } from "./NewPosts";
 import { PostsIndex } from "./PostsIndex";
+import { useState } from "react";
+import axios from "axios";
 
 export function Content() {
   const handlesIndexPosts = () => {
-    console.log("GET That DATA!")
+    axios.get('http://localhost:3000/posts.json')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
   }
-  let posts = [
-    {
-      id: 1,
-      title: "Game 1",
-      date: "Date: month day year",
-      image: "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/1708091/capsule_616x353.jpg?t=1706638660",
-      body: "Here is some information about Halo Infinite!",
-    },
-    {
-      id: 2,
-      title: "Super Contra",
-      date: "Date: month day year",
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbE3qwmloOV1eCR6WLcjSlD5kk36DftHQ-EQ&s",
-      body: "Here is some information about contra",
-    }
-  ];
+  let posts = [];
   return (
     <div>
       <NewPosts />
